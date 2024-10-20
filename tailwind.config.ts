bd1a402de90d510bbf349dacf9228f51d2680f48
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-10-20 12:07:54 +0200

    Start setup, Start building UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
@@ -12,8 +13,39 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        "scale-up-0.8": {
+          "0%": { opacity: "0", transform: "scale(0.8)" },
+          "100%": { opacity: "1", transform: "scale(1)" },
+        },
+      },
+      animation: {
+        "scale-up-0.8": "scale-up-0.8 .5s ease-out forwards",
+      },
+      animationDelay: {
+        "0": "0s",
+        "100": "100ms",
+        "200": "200ms",
+        "300": "300ms",
+        "500": "500ms",
+        "700": "700ms",
+        "1000": "1s",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities, theme }: PluginAPI) {
+      const delays = theme("animationDelay") ?? {};
+      const utilities = Object.entries(delays).map(([key, value]) => {
+        return {
+          [`.animation-delay-${key}`]: {
+            "animation-delay": value,
+          },
+        };
+      });
+
+      addUtilities(utilities);
+    },
+  ],
 };
 export default config;
